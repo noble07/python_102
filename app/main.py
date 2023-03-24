01100bda1e42bfad3cfa9203a68eda6a6f356fcd
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-24 17:24:55 -0500

    Added project with file manipulation and graphics

--- app/main.py
+++ app/main.py
@@ -1,22 +1,20 @@
 import utils # imports the mod file that exist at the same level
-
-data = [
-    {
-      'Country': 'Colombia',
-      'Population': 500
-    },
-    {
-      'Country': 'Bolivia',
-      'Population': 300
-    }
-]
+import read_csv
+import charts
 
 def main():
-  keys, values = dict(utils.get_population())
-  print(keys, values)
+    data = read_csv.read_csv("./app/data.csv")
+    country = input("Type Country => ")
 
-  result = utils.population_by_country(data, 'Colombia')
-  print(result)
+    result = utils.population_by_country(data, country)
+
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
+    
+    c_name, per = utils.get_world_percentages(data)
+    charts.generate_pie_chart(c_name, per)
 
 if __name__ == "__main__": # This validation makes that only this file will be executed if it's run as a script
   main()
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,20 @@
-def get_population():
-    keys = ['col', 'bol']
-    values = [300, 400]
-    return keys, values
+def get_population(country_dict):
+    population_dict = {
+        '2022': int(country_dict["2022 Population"]),
+        '2020': int(country_dict["2020 Population"]),
+        '2015': int(country_dict["2015 Population"]),
+        '2010': int(country_dict["2010 Population"]),
+        '2000': int(country_dict["2000 Population"]),
+        '1990': int(country_dict["1990 Population"]),
+        '1980': int(country_dict["1980 Population"]),
+        '1970': int(country_dict["1970 Population"]) 
+    }
+    return population_dict.keys(), population_dict.values()
 
 def population_by_country(data, country):
-    result = list(filter(lambda item: item['Country'] == country, data))
+    result = list(filter(lambda item: item['Country/Territory'] == country, data))
     return result
+
+def get_world_percentages(data):
+    percentages_dict = {item["Country/Territory"]: item["World Population Percentage"] for item in data}
+    return percentages_dict.keys(), percentages_dict.values()
